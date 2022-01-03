cc4868b581673fd4ff902bac81e7f8a9fa6452a6
Mateusz Brzozowski <mateusz_brzozowski93@wp.pl>
2022-01-03 17:04:55 +0100

    Wprowadzanie przypinania wiadomości z listą na zapisy

--- src/main/pl.mbrzozowski/event/Event.java
+++ src/main/pl.mbrzozowski/event/Event.java
@@ -408,7 +408,7 @@ public class Event {
                         ActiveEvent event = new ActiveEvent(textChannel.getId(), msgID, nameEvent);
                         activeEvents.add(event);
                         addEventDB(event);
-
+                        message.pin().queue();
                         CreateReminder reminder = new CreateReminder(date, time, message.getId());
                         reminder.create();
                     });
