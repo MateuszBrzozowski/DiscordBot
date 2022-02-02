87d289539cb82d659473a65b48add440951abed3
Mateusz Brzozowski <mateusz_brzozowski93@wp.pl>
2022-02-02 02:24:47 +0100

    zmiana interfejsu graficznego, dodano maksymalnie 9 odpowiedzi w ankiecie

--- src/main/pl.mbrzozowski/questionnaire/QuestionnaireBuilder.java
+++ src/main/pl.mbrzozowski/questionnaire/QuestionnaireBuilder.java
@@ -1,5 +1,6 @@
 package questionnaire;
 
+import embed.EmbedSettings;
 import helpers.RoleID;
 import helpers.Users;
 import net.dv8tion.jda.api.EmbedBuilder;
@@ -23,10 +24,17 @@ public class QuestionnaireBuilder {
     private String channelID = null;
     private String question = null;
     private List<String> answers = new ArrayList<>();
-    private String EMOJI_A = ":regional_indicator_a:";
-    private String EMOJI_B = ":regional_indicator_b:";
-    private String EMOJI_C = ":regional_indicator_c:";
-    private String EMOJI_D = ":regional_indicator_d:";
+    private String EMOJI_A = "\uD83C\uDDE6";
+    private String EMOJI_B = "\uD83C\uDDE7";
+    private String EMOJI_C = "\uD83C\uDDE8";
+    private String EMOJI_D = "\uD83C\uDDE9";
+    private String EMOJI_E = "\uD83C\uDDEA";
+    private String EMOJI_F = "\uD83C\uDDEB";
+    private String EMOJI_G = "\uD83C\uDDEC";
+    private String EMOJI_H = "\uD83C\uDDED";
+    private String EMOJI_I = "\uD83C\uDDEE";
+    private String EMOJI_YES = "U+2705";
+    private String EMOJI_NO = "U+274C";
 
     public QuestionnaireBuilder setQuestion(String question) {
         this.question = question;
@@ -39,7 +47,7 @@ public class QuestionnaireBuilder {
     }
 
     public QuestionnaireBuilder addAnswer(String answer) {
-        if (answers.size() < 5) {
+        if (answers.size() < 9) {
             answers.add(answer);
         }
         return this;
@@ -59,9 +67,10 @@ public class QuestionnaireBuilder {
             TextChannel textChannel = jda.getTextChannelById(channelID);
             EmbedBuilder builder = new EmbedBuilder();
             builder.setTitle("Ankieta");
-            builder.setColor(Color.YELLOW);
+            builder.setThumbnail(EmbedSettings.THUMBNAIL);
+            Color questionaire = new Color(59, 136, 195);
+            builder.setColor(questionaire);
             builder.addField("Pytanie", question, false);
-            builder.addBlankField(false);
             builder.setFooter("Utworzono przez " + Users.getUserNicknameFromID(authorID));
 
             builder.addField("Odpowiedzi", getAnswers(), false);
@@ -70,49 +79,86 @@ public class QuestionnaireBuilder {
             textChannel.sendMessage(builder.build()).queue(message -> {
                 MessageEmbed mOld = message.getEmbeds().get(0);
                 String msgID = message.getId();
-                addButtons(message, mOld, msgID);
+                message.editMessage(mOld).setActionRow(
+                        Button.danger("end_" + msgID, "Zakończ")
+                ).queue();
+                addReactions(message, mOld, msgID);
             });
         }
+
     }
 
-    private void addButtons(Message message, MessageEmbed mOld, String msgID) {
+    private void addReactions(Message message, MessageEmbed mOld, String msgID) {
         switch (answers.size()) {
             case 2:
-                message.editMessage(mOld).setActionRow(
-                        Button.primary("A_" + msgID, "A"),
-                        Button.primary("B_" + msgID, "B"),
-                        Button.danger("end_" + msgID, "Zakończ")
-                ).queue();
+                message.addReaction(EMOJI_A).queue();
+                message.addReaction(EMOJI_B).queue();
                 break;
             case 3:
-                message.editMessage(mOld).setActionRow(
-                        Button.primary("A_" + msgID, "A"),
-                        Button.primary("B_" + msgID, "B"),
-                        Button.primary("C_" + msgID, "C"),
-                        Button.danger("end_" + msgID, "Zakończ")
-                ).queue();
+                message.addReaction(EMOJI_A).queue();
+                message.addReaction(EMOJI_B).queue();
+                message.addReaction(EMOJI_C).queue();
                 break;
             case 4:
-                message.editMessage(mOld).setActionRow(
-                        Button.primary("A_" + msgID, "A"),
-                        Button.primary("B_" + msgID, "B"),
-                        Button.primary("C_" + msgID, "C"),
-                        Button.primary("D_" + msgID, "D"),
-                        Button.danger("end_" + msgID, "Zakończ")
-                ).queue();
+                message.addReaction(EMOJI_A).queue();
+                message.addReaction(EMOJI_B).queue();
+                message.addReaction(EMOJI_C).queue();
+                message.addReaction(EMOJI_D).queue();
+                break;
+            case 5:
+                message.addReaction(EMOJI_A).queue();
+                message.addReaction(EMOJI_B).queue();
+                message.addReaction(EMOJI_C).queue();
+                message.addReaction(EMOJI_D).queue();
+                message.addReaction(EMOJI_E).queue();
+                break;
+            case 6:
+                message.addReaction(EMOJI_A).queue();
+                message.addReaction(EMOJI_B).queue();
+                message.addReaction(EMOJI_C).queue();
+                message.addReaction(EMOJI_D).queue();
+                message.addReaction(EMOJI_E).queue();
+                message.addReaction(EMOJI_F).queue();
+                break;
+            case 7:
+                message.addReaction(EMOJI_A).queue();
+                message.addReaction(EMOJI_B).queue();
+                message.addReaction(EMOJI_C).queue();
+                message.addReaction(EMOJI_D).queue();
+                message.addReaction(EMOJI_E).queue();
+                message.addReaction(EMOJI_F).queue();
+                message.addReaction(EMOJI_G).queue();
+                break;
+            case 8:
+                message.addReaction(EMOJI_A).queue();
+                message.addReaction(EMOJI_B).queue();
+                message.addReaction(EMOJI_C).queue();
+                message.addReaction(EMOJI_D).queue();
+                message.addReaction(EMOJI_E).queue();
+                message.addReaction(EMOJI_F).queue();
+                message.addReaction(EMOJI_G).queue();
+                message.addReaction(EMOJI_H).queue();
+                break;
+            case 9:
+                message.addReaction(EMOJI_A).queue();
+                message.addReaction(EMOJI_B).queue();
+                message.addReaction(EMOJI_C).queue();
+                message.addReaction(EMOJI_D).queue();
+                message.addReaction(EMOJI_E).queue();
+                message.addReaction(EMOJI_F).queue();
+                message.addReaction(EMOJI_G).queue();
+                message.addReaction(EMOJI_H).queue();
+                message.addReaction(EMOJI_I).queue();
                 break;
             default:
-                message.editMessage(mOld).setActionRow(
-                        Button.primary("A_" + msgID, "TAK"),
-                        Button.primary("B_" + msgID, "NIE"),
-                        Button.danger("end_" + msgID, "Zakończ")
-                ).queue();
+                message.addReaction(EMOJI_YES).queue();
+                message.addReaction(EMOJI_NO).queue();
         }
     }
 
     private String getAnswers() {
         if (answers.size() < 2) {
-            return "0 | TAK\n0 | NIE";
+            return "TAK **- 0 Głosów**\nNIE **- 0 Głosów**";
         } else {
             return getEmptyPublicAnswersToEmbed();
         }
@@ -122,7 +168,7 @@ public class QuestionnaireBuilder {
         String result = "";
         for (int i = 0; i < answers.size(); i++) {
             result += getEmoji(i + 1);
-            result += " - 0 | " + answers.get(i) + "\n";
+            result += " " + answers.get(i) + " **- 0 Głosów**\n";
         }
         return result;
     }
@@ -137,6 +183,16 @@ public class QuestionnaireBuilder {
                 return EMOJI_C;
             case 4:
                 return EMOJI_D;
+            case 5:
+                return EMOJI_E;
+            case 6:
+                return EMOJI_F;
+            case 7:
+                return EMOJI_G;
+            case 8:
+                return EMOJI_H;
+            case 9:
+                return EMOJI_I;
             default:
                 return "";
         }
