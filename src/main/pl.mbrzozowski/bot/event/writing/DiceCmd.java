aafb3cae2adf8869def548bfed57867d81e5ce41
Mateusz Brzozowski <mateusz_brzozowski93@wp.pl>
2022-04-29 11:45:59 +0200

    Gra kostka tylko na publicznych kanałach

--- src/main/pl.mbrzozowski/bot/event/writing/DiceCmd.java
+++ src/main/pl.mbrzozowski/bot/event/writing/DiceCmd.java
@@ -2,6 +2,7 @@ package bot.event.writing;
 
 import helpers.Commands;
 import model.DiceGame;
+import net.dv8tion.jda.api.entities.ChannelType;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 
 public class DiceCmd extends Proccess {
@@ -12,22 +13,26 @@ public class DiceCmd extends Proccess {
 
     @Override
     public void proccessMessage(Message message) {
-        if (message.getWords()[0].equalsIgnoreCase(Commands.DICE)) {
-            messageReceived.getMessage().delete().submit();
-            if (message.getWords().length == 1) {
-                DiceGame diceGame = new DiceGame();
-                diceGame.playSolo(messageReceived);
-            } else if (message.getWords().length > 1) {
-                DiceGame diceGame = new DiceGame(message.getWords(), messageReceived);
-                getDice().addGame(diceGame);
-            }
-        } else if (!messageReceived.getAuthor().isBot()) {
-            if (getDice().isActiveGameOnChannelID(messageReceived.getChannel().getId())) {
+        if (!messageReceived.isFromType(ChannelType.PRIVATE)){
+            if (message.getWords()[0].equalsIgnoreCase(Commands.DICE)) {
                 messageReceived.getMessage().delete().submit();
-                getDice().play(messageReceived);
-            } else {
-                getNextProccess().proccessMessage(message);
+                if (message.getWords().length == 1) {
+                    DiceGame diceGame = new DiceGame();
+                    diceGame.playSolo(messageReceived);
+                } else if (message.getWords().length > 1) {
+                    DiceGame diceGame = new DiceGame(message.getWords(), messageReceived);
+                    getDice().addGame(diceGame);
+                }
+            } else if (!messageReceived.getAuthor().isBot()) {
+                if (getDice().isActiveGameOnChannelID(messageReceived.getChannel().getId())) {
+                    messageReceived.getMessage().delete().submit();
+                    getDice().play(messageReceived);
+                } else {
+                    getNextProccess().proccessMessage(message);
+                }
             }
+        }else {
+            getNextProccess().proccessMessage(message);
         }
     }
 }
