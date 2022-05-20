f30c442c955012599e16c4c395dafec25aeed656
Mateusz Brzozowski <mateusz_brzozowski93@wp.pl>
2022-05-21 01:34:18 +0200

    Automatyczne przypisanie roli po wejściu na serwer

--- src/main/pl.mbrzozowski/bot/event/Listener.java
+++ src/main/pl.mbrzozowski/bot/event/Listener.java
@@ -1,19 +1,27 @@
 package bot.event;
 
+import helpers.CategoryAndChannelID;
+import helpers.RoleID;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.Role;
+import net.dv8tion.jda.api.entities.UserSnowflake;
 import net.dv8tion.jda.api.events.guild.member.GuildMemberJoinEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import ranger.RangerBot;
+import ranger.Repository;
 
 public class Listener extends ListenerAdapter {
     protected static final Logger logger = LoggerFactory.getLogger(RangerBot.class.getName());
 
     @Override
     public void onGuildMemberJoin(@NotNull GuildMemberJoinEvent event) {
-//        EmbedInfo.sendHelloMessagePrivate(event.getUser().getId());
-//        JDA jda = Repository.getJda();
-//        jda.getTextChannelById(CategoryAndChannelID.CHANNEL_TESTUJEMY).sendMessage("Hey <@" + event.getUser().getId() + ">, welcome to **Rangers Polska!**");
+        JDA jda = Repository.getJda();
+        Guild guild = jda.getGuildById(CategoryAndChannelID.RANGERSPL_GUILD_ID);
+        Role role = guild.getRoleById(RoleID.SQUAD);
+        guild.addRoleToMember(UserSnowflake.fromId(event.getUser().getId()), role).queue();
     }
 }
