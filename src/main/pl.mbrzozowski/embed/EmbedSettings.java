92d28c2faf29ed770be76e5761d5229011901783
Mateusz Brzozowski <mateusz_brzozowski93@wp.pl>
2022-06-13 13:55:50 +0200

    Klasy do przechowywania staych zmienione na interfejsy

--- src/main/pl.mbrzozowski/embed/EmbedSettings.java
+++ src/main/pl.mbrzozowski/embed/EmbedSettings.java
@@ -1,22 +1,22 @@
 package embed;
 
-public class EmbedSettings {
+public interface EmbedSettings {
 
-    public static final String THUMBNAIL = "https://rangerspolska.pl/styles/Hexagon/theme/images/logo.png";
-    public static final String THUMBNAIL_WARNING = "https://cdn.icon-icons.com/icons2/1499/PNG/512/emblemimportant_103451.png";
-    public static final String THUMBNAIL_DICE = "https://www.iconsdb.com/icons/download/white/dice-64.png";
+    String THUMBNAIL = "https://rangerspolska.pl/styles/Hexagon/theme/images/logo.png";
+    String THUMBNAIL_WARNING = "https://cdn.icon-icons.com/icons2/1499/PNG/512/emblemimportant_103451.png";
+    String THUMBNAIL_DICE = "https://www.iconsdb.com/icons/download/white/dice-64.png";
 
-    public static final String RESULT = "WYNIK REKRUTACJI - ";
-    public static final String NAME_LIST = ":white_check_mark: Lista ";
-    public static final String NAME_LIST_RESERVE = ":regional_indicator_r: Rezerwa ";
-    public static final String WHEN_DATE = ":date: Kiedy";
-    public static final String WHEN_TIME = ":clock930: Godzina";
-    public static final String GREEN_CIRCLE = "\uD83D\uDFE2┋";
-    public static final String RED_CIRCLE = "\uD83D\uDD34┋";
-    public static final String BOOK_RED = "\uD83D\uDCD5";
-    public static final String BOOK_BLUE = "\uD83D\uDCD8";
-    public static final String BOOK_GREEN = "\uD83D\uDCD7";
-    public static final String BIN = "\uD83D\uDDD1";
-    public static final String LOCK = "\uD83D\uDD12";
+    String RESULT = "WYNIK REKRUTACJI - ";
+    String NAME_LIST = ":white_check_mark: Lista ";
+    String NAME_LIST_RESERVE = ":regional_indicator_r: Rezerwa ";
+    String WHEN_DATE = ":date: Kiedy";
+    String WHEN_TIME = ":clock930: Godzina";
+    String GREEN_CIRCLE = "\uD83D\uDFE2┋";
+    String RED_CIRCLE = "\uD83D\uDD34┋";
+    String BOOK_RED = "\uD83D\uDCD5";
+    String BOOK_BLUE = "\uD83D\uDCD8";
+    String BOOK_GREEN = "\uD83D\uDCD7";
+    String BIN = "\uD83D\uDDD1";
+    String LOCK = "\uD83D\uDD12";
 }
 
