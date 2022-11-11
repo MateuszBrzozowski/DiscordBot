6c8604a68a8d116c9651aabbcd455b41b639e5cf
Mateusz Brzozowski <mateusz_brzozowski93@wp.pl>
2022-11-11 15:30:22 +0100

    celaning main

--- src/main/java/pl/mbrzozowski/ranger/RangerBot.java
+++ src/main/java/pl/mbrzozowski/ranger/RangerBot.java
@@ -8,44 +8,8 @@ import javax.security.auth.login.LoginException;
 @SpringBootApplication
 public class RangerBot {
 
-
     public static void main(String[] args) throws LoginException {
         SpringApplication.run(RangerBot.class, args);
-
-//        Collection<GatewayIntent> intents = new ArrayList<>();
-//        intents.add(GatewayIntent.GUILD_MEMBERS);
-//        intents.add(GatewayIntent.GUILD_MESSAGES);
-//        intents.add(GatewayIntent.DIRECT_MESSAGES);
-//        intents.add(GatewayIntent.GUILD_MESSAGE_REACTIONS);
-//        JDA jda = JDABuilder.create(BOT_TOKEN, intents)
-//                .addEventListeners(new WriteListener())
-//                .addEventListeners(new ButtonClickListener())
-//                .addEventListeners(new ChannelUpdate())
-//                .addEventListeners(new MessageUpdate())
-//                .addEventListeners(new Listener())
-//                .addEventListeners(new ModalListener())
-//                .addEventListeners(new SelecetMenuListener())
-//                .build();
-//        Repository.setJDA(jda);
-//        jda.getPresence().setActivity(Activity.listening("Spotify"));
-//        jda.getPresence().setStatus(OnlineStatus.ONLINE);
-//        try {
-//            jda.awaitReady();
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
-//        initialize();
-//
-//        RangerLogger.info("Bot uruchomiony.");
     }
 
-
-    private static void initialize() {
-//        Repository.getRecruits().initialize();
-//        Repository.getEvent().initialize();
-//        Repository.getQuestionnaires().initialize();
-//        Repository.getCounter().initialize();
-//        Repository.getServerService().initialize();
-//        Repository.getServerStats().initialize();
-    }
 }
