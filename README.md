ba37180315f8f67c7a2a5e1f31b778ab0f2bf389
Mateusz Brzozowski <mateusz_brzozowski93@wp.pl>
2023-02-08 14:26:25 +0100

    Add README.md

---
+++ README.md
@@ -0,0 +1,19 @@
+## Discord Bot
+
+---
+Bot wspomagający zarządzanie dla organizacji gamingowej 
+
+
+### Wykorzystane technologie
+
+---
+* Java
+* Spring Boot
+* Maven
+* MySql
+* Lombok
+* Mockito
+* Junit 5
+
+#### Java Discord API
+* [DV8FromTheWorld / JDA](https://github.com/DV8FromTheWorld/JDA)
--- src/test/java/pl/mbrzozowski/ranger/event/EventServiceTest.java
+++ src/test/java/pl/mbrzozowski/ranger/event/EventServiceTest.java
@@ -1,21 +1,17 @@
 package pl.mbrzozowski.ranger.event;
 
-import net.dv8tion.jda.api.utils.TimeFormat;
 import org.junit.jupiter.api.Test;
-
-import java.time.LocalDateTime;
-import java.time.ZoneId;
 
 class EventServiceTest {
 
     @Test
-    void main() {
-        //<t:1675015200:F>
-        LocalDateTime dateTime = LocalDateTime.of(2023, 1, 29, 19, 0);
-        long epochSecond = dateTime.atZone(ZoneId.of("Europe/Paris")).toEpochSecond() * 1000;
-        System.out.println("1675015200");
-        System.out.println(epochSecond);
-
-        System.out.println(TimeFormat.DATE_TIME_LONG.format(epochSecond));
+    void test() {
+//        //<t:1675015200:F>
+//        LocalDateTime dateTime = LocalDateTime.of(2023, 1, 29, 19, 0);
+//        long epochSecond = dateTime.atZone(ZoneId.of("Europe/Paris")).toEpochSecond() * 1000;
+//        System.out.println("1675015200");
+//        System.out.println(epochSecond);
+//
+//        System.out.println(TimeFormat.DATE_TIME_LONG.format(epochSecond));
     }
 }
