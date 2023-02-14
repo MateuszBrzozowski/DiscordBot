fa0e699cfae606831a9b1e4842cfadd7147c3892
Mateusz Brzozowski <mateusz_brzozowski93@wp.pl>
2023-02-14 19:29:23 +0100

    Add Tests for EventService

--- src/test/java/pl/mbrzozowski/ranger/event/EventServiceTest.java
+++ src/test/java/pl/mbrzozowski/ranger/event/EventServiceTest.java
@@ -1,10 +1,14 @@
 package pl.mbrzozowski.ranger.event;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import pl.mbrzozowski.ranger.event.reminder.Timers;
 import pl.mbrzozowski.ranger.event.reminder.UsersReminderService;
 import pl.mbrzozowski.ranger.repository.main.EventRepository;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.mockito.Mockito.mock;
 
@@ -21,13 +25,48 @@ class EventServiceTest {
     }
 
     @Test
-    void test() {
-//        //<t:1675015200:F>
-//        LocalDateTime dateTime = LocalDateTime.of(2023, 1, 29, 19, 0);
-//        long epochSecond = dateTime.atZone(ZoneId.of("Europe/Paris")).toEpochSecond() * 1000;
-//        System.out.println("1675015200");
-//        System.out.println(epochSecond);
-//
-//        System.out.println(TimeFormat.DATE_TIME_LONG.format(epochSecond));
+    void getMainList() {
+        Event event = Event.builder().build();
+        Player player = Player.builder().event(event).mainList(true).build();
+        Player player1 = Player.builder().event(event).mainList(false).build();
+        ArrayList<Player> players = new ArrayList<>(List.of(player1, player));
+        event.setPlayers(players);
+        List<Player> resultMainList = eventService.getMainList(event);
+        List<Player> exceptedMainList = new ArrayList<>(List.of(player));
+        Assertions.assertEquals(exceptedMainList,resultMainList);
+    }
+
+    @Test
+    void getMainList_ReturnEmptyList() {
+        Event event = Event.builder().build();
+        Player player = Player.builder().event(event).mainList(false).build();
+        ArrayList<Player> players = new ArrayList<>(List.of(player));
+        event.setPlayers(players);
+        List<Player> resultMainList = eventService.getMainList(event);
+        List<Player> exceptedMainList = new ArrayList<>();
+        Assertions.assertEquals(exceptedMainList,resultMainList);
+    }
+
+    @Test
+    void getReserveList() {
+        Event event = Event.builder().build();
+        Player player = Player.builder().event(event).mainList(true).build();
+        Player player1 = Player.builder().event(event).mainList(false).build();
+        ArrayList<Player> players = new ArrayList<>(List.of(player1, player));
+        event.setPlayers(players);
+        List<Player> resultMainList = eventService.getReserveList(event);
+        List<Player> exceptedMainList = new ArrayList<>(List.of(player1));
+        Assertions.assertEquals(exceptedMainList,resultMainList);
+    }
+
+    @Test
+    void getReserveList_ReturnEmptyList() {
+        Event event = Event.builder().build();
+        Player player = Player.builder().event(event).mainList(true).build();
+        ArrayList<Player> players = new ArrayList<>(List.of(player));
+        event.setPlayers(players);
+        List<Player> resultMainList = eventService.getReserveList(event);
+        List<Player> exceptedMainList = new ArrayList<>();
+        Assertions.assertEquals(exceptedMainList,resultMainList);
     }
 }
