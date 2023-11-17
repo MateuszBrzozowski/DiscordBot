c09efd20390d397649aa8407551fe8bdacfa202a
Mateusz Brzozowski <mateusz_brzozowski93@wp.pl>
2023-11-17 15:27:38 +0100

    Add log info for developer cmd

--- src/main/java/pl/mbrzozowski/ranger/bot/events/writing/DeveloperCmd.java
+++ src/main/java/pl/mbrzozowski/ranger/bot/events/writing/DeveloperCmd.java
@@ -1,10 +1,13 @@
 package pl.mbrzozowski.ranger.bot.events.writing;
 
+import lombok.extern.slf4j.Slf4j;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import org.jetbrains.annotations.NotNull;
 import pl.mbrzozowski.ranger.event.EventService;
 import pl.mbrzozowski.ranger.helpers.Commands;
 import pl.mbrzozowski.ranger.model.BotWriter;
 
+@Slf4j
 public class DeveloperCmd extends Proccess {
 
     private final EventService eventService;
@@ -17,21 +20,28 @@ public class DeveloperCmd extends Proccess {
     }
 
     @Override
-    public void proccessMessage(Message message) {
+    public void proccessMessage(@NotNull Message message) {
         if (message.getWords().length == 2 && message.getWords()[0].equalsIgnoreCase(Commands.DISABLE_BUTTONS)) {
             eventService.disableButtons(message.getWords()[1]);
+            log.info("{} - disable buttons (msg={})", messageReceived.getAuthor(), message.getContentDisplay());
         } else if (message.getWords().length == 3 && message.getWords()[0].equalsIgnoreCase(Commands.DISABLE_BUTTONS)) {
             eventService.disableButtons(message.getWords()[1], message.getWords()[2]);
+            log.info("{} - disable buttons (msg={})", messageReceived.getAuthor(), message.getContentDisplay());
         } else if (message.getWords().length == 2 && message.getWords()[0].equalsIgnoreCase(Commands.ENABLE_BUTTONS)) {
             eventService.enableButtons(message.getWords()[1]);
+            log.info("{} - enable buttons (msg={})", messageReceived.getAuthor(), message.getContentDisplay());
         } else if (message.getWords().length == 3 && message.getWords()[0].equalsIgnoreCase(Commands.ENABLE_BUTTONS)) {
             eventService.enableButtons(message.getWords()[1], message.getWords()[2]);
+            log.info("{} - enable buttons (msg={})", messageReceived.getAuthor(), message.getContentDisplay());
         } else if (message.getWords().length == 2 && message.getWords()[0].equalsIgnoreCase(Commands.MSG)) {
             botWriter.setChannelID(message.getWords()[1]);
+            log.info("{} - set channel id for send message", messageReceived.getAuthor());
         } else if (message.getWords().length == 1 && message.getWords()[0].equalsIgnoreCase(Commands.MSG_CANCEL)) {
             botWriter.setActive(false);
+            log.info("{} - cancel sending message", messageReceived.getAuthor());
         } else if (botWriter.isActive()) {
             botWriter.sendMsg(messageReceived.getMessage().getContentDisplay());
+            log.info("{} - send message as RangerBot", messageReceived.getAuthor());
         } else {
             getNextProccess().proccessMessage(message);
         }
--- src/main/java/pl/mbrzozowski/ranger/bot/events/writing/InvalidCmd.java
+++ src/main/java/pl/mbrzozowski/ranger/bot/events/writing/InvalidCmd.java
@@ -1,8 +1,12 @@
 package pl.mbrzozowski.ranger.bot.events.writing;
 
+import lombok.extern.slf4j.Slf4j;
+import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import org.jetbrains.annotations.NotNull;
 import pl.mbrzozowski.ranger.DiscordBot;
 
+@Slf4j
 public class InvalidCmd extends Proccess {
 
     public InvalidCmd(MessageReceivedEvent messageReceived) {
@@ -10,12 +14,18 @@ public class InvalidCmd extends Proccess {
     }
 
     @Override
-    public void proccessMessage(Message message) {
-        DiscordBot.getJda().getUserById(message.getUserID()).openPrivateChannel().queue(privateChannel -> {
-            privateChannel.sendMessage("Niestety, nie rozumiem Ciebie. \n" +
-                    "**!event** - otwiera generator eventów.\n" +
-                    "**!eventsEdit** - edytowanie aktywnych eventów.\n" +
-                    "Jeżeli potrzebujesz więcej pomocy. Wpisz **!help**").queue();
-        });
+    public void proccessMessage(@NotNull Message message) {
+        User user = DiscordBot.getJda().getUserById(message.getUserID());
+        if (user != null) {
+            user.openPrivateChannel().queue(privateChannel -> {
+                privateChannel.sendMessage("""
+                                Niestety, nie rozumiem Ciebie.\s
+                                **!event** - otwiera generator eventów.
+                                **!eventsEdit** - edytowanie aktywnych eventów.
+                                Jeżeli potrzebujesz więcej pomocy. Wpisz **!help**""")
+                        .queue(message1 -> log.info("{} - misunderstood message", messageReceived.getAuthor()));
+            });
+        }
+        log.error("{} - misunderstood message. User is null", messageReceived.getAuthor());
     }
 }
