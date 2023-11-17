c403d505a924128d14e6c3bffe0cf891e2eb6c10
Mateusz Brzozowski <mateusz_brzozowski93@wp.pl>
2023-11-17 13:11:54 +0100

    Change log info for LogChannel.java

--- src/main/java/pl/mbrzozowski/ranger/bot/events/writing/LogChannel.java
+++ src/main/java/pl/mbrzozowski/ranger/bot/events/writing/LogChannel.java
@@ -1,9 +1,11 @@
 package pl.mbrzozowski.ranger.bot.events.writing;
 
+import lombok.extern.slf4j.Slf4j;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import pl.mbrzozowski.ranger.response.EmbedInfo;
 import pl.mbrzozowski.ranger.helpers.CategoryAndChannelID;
 
+@Slf4j
 public class LogChannel extends Proccess {
 
     public LogChannel(MessageReceivedEvent messageReceived) {
@@ -13,6 +15,7 @@ public class LogChannel extends Proccess {
     @Override
     public void proccessMessage(Message message) {
         if (messageReceived.getChannel().getId().equalsIgnoreCase(CategoryAndChannelID.CHANNEL_RANGER_BOT_LOGGER)) {
+            log.info(messageReceived.getAuthor() + " - wrote on Ranger log channel");
             EmbedInfo.noWriteOnLoggerChannel(messageReceived);
         } else {
             getNextProccess().proccessMessage(message);
