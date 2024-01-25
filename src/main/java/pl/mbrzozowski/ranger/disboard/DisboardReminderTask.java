c5e75ce18d6b370b414d666c1271b2b1b1ab6cbe
Mateusz Brzozowski <mateusz_brzozowski93@wp.pl>
2024-01-25 14:33:04 +0100

    add dynamic message for disboard reminder

--- src/main/java/pl/mbrzozowski/ranger/disboard/DisboardReminderTask.java
+++ src/main/java/pl/mbrzozowski/ranger/disboard/DisboardReminderTask.java
@@ -5,6 +5,9 @@ import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
 import pl.mbrzozowski.ranger.DiscordBot;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 import java.util.TimerTask;
 
 @Slf4j
@@ -12,6 +15,22 @@ public class DisboardReminderTask extends TimerTask {
 
     static final String CHANNEL_ID = "1154107337347448872";
     private final DisboardService disboardService;
+    private final List<String> reqMessages = new ArrayList<>(List.of(
+            "Witaj Sir, Użyj proszę komendy **/bump** aby podbić nasz serwer.",
+            "Siema mordeczki. Mógłby ktoś z was użyć komendy **/bump** aby podbić nasz serwer?",
+            "Siema mordeczki. We no ktoś podbij komendą **/bump**",
+            "Podbij ktoś nasz serwer komendą **/bump**. Dzięki!",
+            "Mistrzuniu podbij no nasz serwer komendą **/bump**. Dzięki",
+            "Szukam dobrej duszyczki co podbije nasz serwer komendą **/bump**",
+            "Podbij nasz serwer komendą **/bump**",
+            "Weź no użyj komendy **/bump**",
+            "Podbić musze bo się uduszę. Komenda **/bump** w ten tychmiast",
+            "Podbić musze bo się uduszę. Użyj komendy **/bump**",
+            "Księciuniu poratujesz komendą **/bump**?",
+            "Haloo!!! Prośbę mam. Podbij serwer komendą **/bump**?",
+            "Podbij nasz serwer komendą **/bump**?",
+            "Podbij no nasz serwer komendą **/bump**"
+    ));
 
     public DisboardReminderTask(DisboardService disboardService) {
         this.disboardService = disboardService;
@@ -31,12 +50,22 @@ public class DisboardReminderTask extends TimerTask {
             if (jda != null) {
                 TextChannel channel = jda.getTextChannelById(CHANNEL_ID);
                 if (channel != null) {
-                    channel.sendMessage("Witaj Sir, Użyj proszę komendy **/bump** aby podbić nasz serwer.").queue(disboardService::setReqMessage);
+                    channel.sendMessage(getMessage()).queue(disboardService::setReqMessage);
                     disboardService.sentBumpReminder();
                     return;
                 }
             }
             attempt++;
         }
+    }
+
+    private String getMessage() {
+        Random random = new Random();
+        int index = random.nextInt(reqMessages.size());
+        return getMessage(index);
+    }
+
+    private String getMessage(int index) {
+        return reqMessages.get(index);
     }
 }
