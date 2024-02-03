fe7337704ca821917b77484ad1256177ad211922
Mateusz Brzozowski <mateusz_brzozowski93@wp.pl>
2024-02-03 10:09:16 +0100

    event create and settings permission

--- src/main/java/pl/mbrzozowski/ranger/event/EventService.java
+++ src/main/java/pl/mbrzozowski/ranger/event/EventService.java
@@ -30,7 +30,6 @@ import pl.mbrzozowski.ranger.repository.main.EventRepository;
 import pl.mbrzozowski.ranger.response.EmbedInfo;
 import pl.mbrzozowski.ranger.response.EmbedSettings;
 import pl.mbrzozowski.ranger.response.ResponseMessage;
-import pl.mbrzozowski.ranger.settings.SettingsService;
 
 import java.time.LocalDateTime;
 import java.time.ZoneId;
@@ -44,17 +43,14 @@ public class EventService {
 
     private static final int MAX_EVENTS = 25;
     private final UsersReminderService usersReminderService;
-    private final SettingsService settingsService;
     private final EventRepository eventRepository;
     private final Timers timers;
 
     @Autowired
     public EventService(UsersReminderService usersReminderService,
                         EventRepository eventRepository,
-                        SettingsService settingsService,
                         Timers timers) {
         this.eventRepository = eventRepository;
-        this.settingsService = settingsService;
         this.timers = timers;
         this.usersReminderService = usersReminderService;
         setReminders();
@@ -480,10 +476,8 @@ public class EventService {
         commandData.add(Commands.slash(EVENT_DELETE_CHANNEL_TACTICAL_DELAY.getName(), EVENT_DELETE_CHANNEL_TACTICAL_DELAY.getDescription())
                 .setDefaultPermissions(DefaultMemberPermissions.enabledFor(Permission.MANAGE_CHANNEL))
                 .addOption(OptionType.INTEGER, "days", "Po ilu dniach?", true));
-        commandData.add(Commands.slash(EVENT_CREATE.getName(), EVENT_CREATE.getDescription())
-                .setDefaultPermissions(DefaultMemberPermissions.enabledFor(Permission.MANAGE_CHANNEL)));
-        commandData.add(Commands.slash(EVENT_SETTINGS.getName(), EVENT_SETTINGS.getDescription())
-                .setDefaultPermissions(DefaultMemberPermissions.enabledFor(Permission.MANAGE_CHANNEL)));
+        commandData.add(Commands.slash(EVENT_CREATE.getName(), EVENT_CREATE.getDescription()));
+        commandData.add(Commands.slash(EVENT_SETTINGS.getName(), EVENT_SETTINGS.getDescription()));
         commandData.add(Commands.slash(FIX_EVENT_EMBED.getName(), FIX_EVENT_EMBED.getDescription())
                 .addOption(OptionType.STRING, "id", "id wiadomości", true)
                 .setDefaultPermissions(DefaultMemberPermissions.enabledFor(Permission.MANAGE_CHANNEL)));
