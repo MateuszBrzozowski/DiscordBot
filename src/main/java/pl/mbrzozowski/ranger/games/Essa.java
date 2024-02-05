d6f4117e0f702240e354c28bc1cf32e2bc3efa20
Mateusz Brzozowski <mateusz_brzozowski93@wp.pl>
2024-02-05 10:07:34 +0100

    fix essa game

--- src/main/java/pl/mbrzozowski/ranger/games/Essa.java
+++ src/main/java/pl/mbrzozowski/ranger/games/Essa.java
@@ -1,47 +1,40 @@
 package pl.mbrzozowski.ranger.games;
 
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import org.apache.commons.lang3.StringUtils;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 import pl.mbrzozowski.ranger.helpers.Math;
 
 import java.time.LocalDate;
 import java.util.HashSet;
+import java.util.Optional;
 import java.util.Random;
-import java.util.Set;
 
 public class Essa {
 
-    private static final Set<EssaPlayer> players = new HashSet<>();
+    private static final HashSet<EssaPlayer> players = new HashSet<>();
 
     public static void start(@NotNull SlashCommandInteractionEvent event) {
         EssaPlayer essaPlayer = new EssaPlayer(LocalDate.now(), event.getUser().getId(), 0);
-        EssaPlayer existPlayer = isExist(essaPlayer);
-        if (existPlayer == null) {
+        Optional<EssaPlayer> existPlayer = isExist(essaPlayer);
+        if (existPlayer.isEmpty()) {
             int number = drawNumber();
             number = roundTo5(number);
             essaPlayer.setEssaValue(number);
             players.add(essaPlayer);
             showResult(event, number);
         } else {
-            showResult(event,existPlayer.getEssaValue());
+            showResult(event, existPlayer.get().getEssaValue());
         }
     }
 
-    @Nullable
-    protected static EssaPlayer isExist(EssaPlayer essaPlayer) {
-        if (players.contains(essaPlayer)) {
-            for (EssaPlayer player : players) {
-                if (player.equals(essaPlayer)) {
-                    if (player.getDate().equals(essaPlayer.getDate())) {
-                        return player;
-                    } else {
-                        players.remove(player);
-                    }
-                }
-            }
-        }
-        return null;
+    @NotNull
+    protected static Optional<EssaPlayer> isExist(EssaPlayer essaPlayer) {
+        players.removeIf(player -> player.getDate() != null && !player.getDate().equals(essaPlayer.getDate()));
+        return players
+                .stream()
+                .filter(player -> StringUtils.isNotBlank(player.getUserId()) && player.getUserId().equals(essaPlayer.getUserId()))
+                .findFirst();
     }
 
     private static void showResult(@NotNull SlashCommandInteractionEvent event, int number) {
