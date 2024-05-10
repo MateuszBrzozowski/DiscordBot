e8edb2105e05e0714ff6a6a59444d965e4185f6d
Mateusz Brzozowski <mateusz_brzozowski93@wp.pl>
2024-05-10 13:09:00 +0200

    Fix table name and change port

--- src/main/java/pl/mbrzozowski/ranger/recruit/Recruit.java
+++ src/main/java/pl/mbrzozowski/ranger/recruit/Recruit.java
@@ -14,7 +14,7 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-@Entity
+@Entity(name = "recruit")
 public class Recruit {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
